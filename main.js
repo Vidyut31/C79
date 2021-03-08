40daff6c29a5545507e5dcdbdf876d4c4e890600
Vidyut31 <64831963+Vidyut31@users.noreply.github.com>
2021-03-08 15:17:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,26 @@
+	menu_list_array = ["Chicken Tandoori Pizza", "Veg Supreme Pizza", "Paneer Tikka Pizza", "Deluxe Veggie Pizza", "Veg Extravaganza Pizza"];
 
-	name_of_the_student_array = [];
-	
-	function submit()
-	{
-        
-        var name_1 = document.getElementById("name_of_the_student_1").value;
-        var name_2 = document.getElementById("name_of_the_student_2").value;
-        var name_3 = document.getElementById("name_of_the_student_3").value;
-        var name_4 = document.getElementById("name_of_the_student_4").value;
-
-        name_of_the_student_array.push(name_1);
-        name_of_the_student_array.push(name_2);
-        name_of_the_student_array.push(name_3);
-        name_of_the_student_array.push(name_4);
-
-		console.log(name_of_the_student_array);
-		
-        document.getElementById("display_name").innerHTML = name_of_the_student_array;
-        document.getElementById("submit_button").style.display = "none";
-        document.getElementById("sort_button").style.display = "inline-block";
-
+	function getmenu() {
+	    var htmldata;
+	    htmldata = "<ol class='menulist'>"
+	    menu_list_array.sort();
+	    for (var i = 0; i < menu_list_array.length; i++) {
+	        htmldata = htmldata + '<li>' + menu_list_array[i] + '</li>'
+	    }
+	    htmldata = htmldata + "</ol>"
+	    document.getElementById("display_menu").innerHTML = htmldata;
 	}
 
-function sorting()
-{
-	name_of_the_student_array.sort();
-    console.log(name_of_the_student_array);
-	document.getElementById("display_name").innerHTML = name_of_the_student_array;
-}
+	function add_item() {
+	    var htmldata;
+	    var item = document.getElementById("add_item").value;
+	    menu_list_array.push(item);
+	    menu_list_array.sort();
+	    htmldata = "<section class='cards'>"
+	    for (var i = 0; i < menu_list_array.length; i++) {
+
+	        htmldata = htmldata + '<div class="card">' + '<img src="images/pizzaImg.png"/>' + menu_list_array[i] + '</div>'
+	    }
+	    htmldata = htmldata + "</section>"
+	    document.getElementById("display_addedmenu").innerHTML = htmldata;
+	}
